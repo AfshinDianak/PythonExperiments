375a86f8026b88ce42c666694184d913827813d8
Afshin Dianak Shori <afshin.dianakshori@gmail.com>
2026-05-22 10:08:37 +0100

    Update functions.py

--- PythonTempExperiments/functions.py
+++ PythonTempExperiments/functions.py
@@ -1,3 +1,4 @@
+####################################################################################################
 #This is the file that contains all the common functions to be used in operational scripts
 def Add(num1, num2):
     Total = num1 + num2
