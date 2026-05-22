69347543f13ccda8172b8d8ff701c1ac8fb54b90
Afshin Dianak Shori <Afshin.DianakShori@royallondon.com>
2026-05-22 10:04:10 +0100

    new simple function added

--- PythonTempExperiments/functions.py
+++ PythonTempExperiments/functions.py
@@ -1,4 +1,7 @@
 #This is the file that contains all the common functions to be used in operational scripts
+def Add(num1, num2):
+    Total = num1 + num2
+    return Total
 
 def Print(message):
     print(message)
