563c31b8bfc9bf5303dcec179047bbce1595cd0e
Afshin Dianak Shori <Afshin.DianakShori@royallondon.com>
2026-05-26 07:52:08 +0100

    new test

--- PythonTempExperiments/syntax_variables.py
+++ PythonTempExperiments/syntax_variables.py
@@ -1,6 +1,6 @@
 #This file is for practicing and learning scripting with python to become more efficient with DevOps
 #Please use the functions file for creating new functions.
-
+#This line is added to test merge conflict.
 print("******This file is for practicing and learning scripting with python to become more efficient with DevOps*****")
 age = 20
 name ="Ed"
